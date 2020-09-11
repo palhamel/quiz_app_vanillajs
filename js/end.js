56365cc32f5519dcac4d91e458d2adc91eac7c2f
Pål Hamel <pal.hamel@gmail.com>
2020-09-11 20:28:14 +0200

    save score to array

--- js/end.js
+++ js/end.js
@@ -2,18 +2,31 @@ const username = document.getElementById("username");
 const saveScoreBtn = document.getElementById("saveScoreBtn");
 const finalScore = document.getElementById("finalScore");
 const mostRecentScore = localStorage.getItem("mostRecentScore");
-  console.log("Your score will be :", mostRecentScore);
-finalScore.innerText = mostRecentScore;
+// console.log("Your score will be :", mostRecentScore);
 
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+console.log(highScores);
+
+finalScore.innerText = mostRecentScore;
 
 username.addEventListener("keyup", () => {
   // console.log(username.value);
   saveScoreBtn.disabled = !username.value;
-  
-})
+});
 
 saveHighScore = (e) => {
   console.log("clicked the button!");
   // prevent the form from refreshing - to display console:
   e.preventDefault();
+
+  const score = {
+    score: mostRecentScore,
+    name: username.value,
+  };
+  highScores.push(score)
+  // sort top 5 score in array:
+
+
+
+  console.log(highScores);
 };
