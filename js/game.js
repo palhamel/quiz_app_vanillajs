591aaa9139d654fe4855d47b2b84b28a461d18f0
Pål Hamel <pal.hamel@gmail.com>
2020-09-12 14:33:09 +0200

    add json file, fetch and catch errors

--- js/game.js
+++ js/game.js
@@ -1,4 +1,4 @@
-console.log("🎾 -----Quizz----- 🎾");
+// console.log("🎾 -----Quizz----- 🎾");
 
 const question = document.getElementById("question");
 const choices = Array.from(document.getElementsByClassName("choice-text"));
@@ -15,33 +15,22 @@ let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
 
-let questions = [
-  {
-    question: "Inside which HTML element do we put the JavaScript??",
-    choice1: "<script>",
-    choice2: "<javascript>",
-    choice3: "<js>",
-    choice4: "<scripting>",
-    answer: 1,
-  },
-  {
-    question:
-      "What is the correct syntax for referring to an external script called 'xxx.js'?",
-    choice1: "<script href='xxx.js'>",
-    choice2: "<script name='xxx.js'>",
-    choice3: "<script src='xxx.js'>",
-    choice4: "<script file='xxx.js'>",
-    answer: 3,
-  },
-  {
-    question: " How do you write 'Hello World' in an alert box?",
-    choice1: "msgBox('Hello World');",
-    choice2: "alertBox('Hello World');",
-    choice3: "msg('Hello World');",
-    choice4: "alert('Hello World');",
-    answer: 4,
-  },
-];
+let questions = [];
+
+fetch("../json/questions.json")
+  .then((res) => {
+    // console.log("from json;", res);
+    return res.json();
+  })
+  .then((loadedQuestions) => {
+    // console.log(loadedQuestions);
+    questions = loadedQuestions;
+    startGame();
+  })
+  .catch(err => {
+    console.log(" ❌ Houston, we have a problem:", err);
+  });
+
 
 // constants:
 const CORRECT_BONUS = 10;
@@ -58,7 +47,7 @@ startGame = () => {
 // pick up a question:
 getNewQuestion = () => {
   if (availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
-    localStorage.setItem("mostRecentScore", score)
+    localStorage.setItem("mostRecentScore", score);
     // then go to end page:
 
     return window.location.assign("../end.html");
@@ -127,7 +116,5 @@ incrementScore = (num) => {
   score += num;
   scoreText.innerText = score;
 };
-
-startGame();
 
 // console.log("question counter:", questionCounter);
